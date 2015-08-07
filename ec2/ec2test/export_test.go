6f75dfe25b0009a47ffd316510a88f8985847fea
Dimiter Naydenov <dimiter.naydenov@canonical.com>
2015-08-07 14:27:44 +0300

    AddVPC, UpdateVPC, RemoveVPC, AddDefaultVPCAndSubnets added and tested.

---
+++ ec2/ec2test/export_test.go
@@ -0,0 +1,15 @@
+//
+// goamz - Go packages to interact with the Amazon Web Services.
+//
+//   https://wiki.ubuntu.com/goamz
+//
+// Copyright (c) 2015 Canonical Ltd.
+//
+
+package ec2test
+
+var (
+	AddInternetGateway   = &addInternetGateway
+	AddSubnet            = &addSubnet
+	SetAccountAttributes = &setAccountAttributes
+)
